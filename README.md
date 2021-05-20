97515eca68e6fc954dc64e45e9830c3157c82b04
Graooo <graodinosaurcode@gmail.com>
2021-05-20 21:03:12 +0700

    deploy

---
+++ README.md
@@ -0,0 +1,11 @@
+# React Redux Cart
+
+_A simple Cart built by Redux and React JS_
+
+# Features
+
+- Add to cart
+- Remove from cart
+- All items in cart are stored by realtime database on fire base
+
+Link demo: https://grao-redux-cart.web.app/
---
+++ src/store/ui-slice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const uiSlice = createSlice({
+  name: "ui",
+  initialState: { show: false, notification: null },
+  reducers: {
+    toggle(state) {
+      state.show = !state.show;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+  },
+});
+
+export const uiActions = uiSlice.actions;
+
+export default uiSlice.reducer;
